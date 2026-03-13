653654d80e7f7a884ba7a51b9658e9752bb211da
Nnamdi Joseph Asouzu <61297245+asouzujoseph@users.noreply.github.com>
2026-03-12 20:31:38 -0400

    Add files via upload

--- Scripts/run_fastqc.sh
+++ Scripts/run_fastqc.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-# Load conda/mamba into this script
+
+# Load mamba inside script
 source ~/.local/share/mamba/etc/profile.d/mamba.sh
 mamba activate rnaseq
 
 RAW=/mnt/c/Users/User/Documents/Damola/rawData
 OUT=/mnt/c/Users/User/Documents/Damola/fastqc_raw
 
-mkdir -p $OUT
+mkdir -p "$OUT"
 
-for fq in $RAW/*.fastq.gz
-do
-    echo "Running FastQC on $fq"
-    fastqc $fq -o $OUT
-done
+echo "Running FastQC on RAW reads using 16 CPUs..."
+
+ls $RAW/*.fastq.gz | parallel -j 16 "fastqc {} -o $OUT"
+
+echo "FastQC (raw) complete."
 
 mamba deactivate
